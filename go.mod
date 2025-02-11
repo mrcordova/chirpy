eb6e122c0e9b2b6fb26109081ea42ddefe664e5c
mrcordova <46950394+mrcordova@users.noreply.github.com>
2025-02-10 16:55:10 -0800

    create server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mrcordova/chirpy
+
+go 1.23.5
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+)
+type apiHandler struct{}
+
+func (apiHandler) ServeHTTP(http.ResponseWriter, *http.Request) {}
+func main() {
+	mux := http.NewServeMux()
+	// mux.Handle("/api/", apiHandler{})
+	// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	if r.URL.Path != "/" {
+	// 		http.NotFound(w, r)
+	// 		return
+	// 	}
+	// 	// fmt.Fprintf(w, "Welcome to the home page!")
+	// })
+	server := http.Server{Handler: mux}
+	server.Addr = ":8080"
+	server.ListenAndServe()
+}
