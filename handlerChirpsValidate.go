4e8def4530a922650d7a0acb6e3994a1d1c6897f
mrcordova <46950394+mrcordova@users.noreply.github.com>
2025-02-11 20:28:54 -0800

    clean words in chirps validate

--- handlerChirpsValidate.go
+++ handlerChirpsValidate.go
@@ -3,15 +3,18 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
+
 
 func handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 		Body string `json:"body"`
 	}
 	type returnVals struct {
-		Valid bool `json:"valid"`
+		Cleaned_Body string `json:"cleaned_body"`
 	}
+	profaneWords := map[string]string{"kerfuffle": "****", "sharbert": "****","fornax": "****"}
 
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
@@ -27,7 +30,29 @@ func handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	
+	bodySlice := strings.Fields(params.Body)
+	for i, word := range bodySlice {
+		lowercaseWord := strings.ToLower(word)
+		if val, ok := profaneWords[lowercaseWord]; ok {
+			bodySlice[i] = val
+		}
+	}
+	cleaned := getCleanedBody(params.Body, profaneWords)
+
 	respondWithJSON(w, http.StatusOK, returnVals{
-		Valid: true,
+		Cleaned_Body: cleaned,
 	})
+}
+
+func getCleanedBody(body string, badWords map[string]string) string {
+	words := strings.Split(body, " ")
+	for i, word := range words {
+		loweredWord := strings.ToLower(word)
+		if val, ok := badWords[loweredWord]; ok {
+			words[i] = val
+		}
+	}
+	cleaned := strings.Join(words, " ")
+	return cleaned
 }
